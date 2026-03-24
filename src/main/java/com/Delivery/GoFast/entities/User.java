11cf422e896a50b18a36df31749f294ec6c24d2e
tayssobrt <gui.andrevieira@gmail.com>
2026-03-24 13:35:41 -0300

    Feat: entity User creation

---
+++ src/main/java/com/Delivery/GoFast/entities/User.java
@@ -0,0 +1,32 @@
+package com.Delivery.GoFast.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "phone")
+    private String phone;
+}
