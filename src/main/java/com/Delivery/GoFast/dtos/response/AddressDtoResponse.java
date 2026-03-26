c737d56d2683197b314d3a68c309de3e2b6b3c23
tayssobrt <gui.andrevieira@gmail.com>
2026-03-25 22:11:15 -0300

    Update: Creating Dto

--- src/main/java/com/Delivery/GoFast/dtos/response/AddressDtoResponse.java
+++ src/main/java/com/Delivery/GoFast/dtos/response/AddressDtoResponse.java
@@ -1,5 +1,13 @@
 package com.Delivery.GoFast.dtos.response;
 
 public record AddressDtoResponse(
-
+        Long id,
+        Long cep,
+        String street,
+        String city,
+        String state,
+        String neighborhood,
+        String number,
+        String complement,
+        UserDtoResponse user
 ) {}
